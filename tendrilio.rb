2e1bc739800bca6e360813d18cb520ef96d86c5a
Ken Kaiser <kenny.kaiser@gmail.com>
2012-02-04 14:40:57 -0500

    basic twilio setup

--- tendrilio.rb
+++ tendrilio.rb
@@ -1,12 +1,35 @@
 # tendrilio.rb
 require 'rubygems'
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
   @title = "Tendrilio"
   erb :index, :layout => :layout
 end
 
-get '/auth' do
+get '/configure' do
+  erb :configure
+end
+
+get '/request' do
+  redirect '/smstest'
+end
+
+get '/smstest' do
+  # Twilio credentials
+  account_sid = ENV['TWILIO_SID']
+  auth_token = ENV['TWILIO_TOKEN']
+  caller_id = ENV['TWILIO_CALLER_ID']
   
+  # set up a client to talk to the Twilio REST API
+  @client = Twilio::REST::Client.new account_sid, auth_token
+  
+  @client.account.sms.messages.create(
+    :from => caller_id,
+    :to => '+19546090220',
+    :body => 'Hey there!'
+  )
+  
+  redirect('/')
 end
